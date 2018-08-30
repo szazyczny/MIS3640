85ffca2b9018d3e6c75be3a60548db1b4dd7ef65
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-08-30 17:23:11 -0400

    Exercise 1 - Completed

--- Session01/hello.py
+++ Session01/hello.py
@@ -1,25 +1,25 @@
 print('Hello, Sarah!')
 
-#E1 Q1a
-#print('Hello, Sarah!' 
-#E1 Q1b
-#print'Hello, Sarah! 
+#E1 Q1a - missing one ()
+print('Hello, Sarah!' 
+#E1 Q1b - missing both ()
+print'Hello, Sarah! 
 
-#E1 Q2a
-#print('Hello, Sarah!)
-#E1 Q2b
-#print(Hello, Sarah!)
-#E1 Q2c
-#pint('Hello, Sarah!')
+#E1 Q2a - missing one '
+print('Hello, Sarah!)
+#E1 Q2b - missing both '
+print(Hello, Sarah!)
+#E1 Q2c - misspelling print
+pint('Hello, Sarah!')
 
-#print(-2)
-#E1 Q3a
-#print(+2)
-#E1 Q3b
-#print(2++2)
+print(-2)
+#E1 Q3a - adding + before integer
+print(+2)
+#E1 Q3b - double ++
+print(2++2)
 
-#E1 Q4
-#print(02)
+#E1 Q4 - adding 0 before integer
+print(02)
 
-#E1 Q5
+#E1 Q5 - no operator
 print(2 4)
