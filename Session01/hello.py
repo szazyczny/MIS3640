81c7bfa5b63b7a7a9f9a40c0dc61ea4f53a9915b
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-08-30 17:12:19 -0400

    E1 Q2b
    File "hello.py", line 6
        print'Hello, Sarah!
            ^
    SyntaxError: invalid syntax

--- Session01/hello.py
+++ Session01/hello.py
@@ -1,3 +1,26 @@
 print('Hello, Sarah!')
 
-print('Hello, Sarah!'
+#E1 Q1a
+print('Hello, Sarah!' 
+#E1 Q1b
+print'Hello, Sarah! 
+
+#E1 Q2a
+print('Hello, Sarah!)
+#E1 Q2b
+print(Hello, Sarah!)
+#E1 Q2c
+pint('Hello, Sarah!')
+
+#E1 Q3a
+print(-2)
+#E1 Q3b
+print(+2)
+#E1 Q3c
+print(2++2)
+
+#E1 Q4
+print(02)
+
+#E1 Q5
+print(2 4)
