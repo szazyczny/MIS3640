cd0d61b191c2038aa6994c4de2342ce65218513e
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-08-31 18:09:17 -0400

    Exercise 2 - Completed

--- Session01/calc.py
+++ Session01/calc.py
@@ -1,1 +1,14 @@
-print(8 + 30 + 2018)
+#print(8 + 30 + 2018)
+
+#E2 Q1 - how many seconds = 2562 seconds
+print((42 * 60) + 42)
+
+#E2 Q2 - how many miles is 10 km = 6.25 miles
+print(10 / 1.6)
+
+#E2 Q3a - average pace in miles per second = 409.92 mps
+print(2562 / 6.25)
+#E2 Q3b - average pace in miles per minute = 6.832 mpm
+print(409.92 / 60)
+#E2 Q3c - average pace in miles per hour = 8.782201405152225 mph
+print(60 / 6.832)
