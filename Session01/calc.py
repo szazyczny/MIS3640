baf67ee1e341450521573e873a940de4d1e33bc3
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-09-05 17:14:14 -0400

    Session 2 Exercises Completed

--- Session01/calc.py
+++ Session01/calc.py
@@ -16,8 +16,8 @@
 # print(60 / 6.832)
 
 #SESSION 2 EXERCISES
-#The volume of a sphere with radius r is (4/3)πr^3. 
-# What is the volume of a sphere with radius 5?
+#1. The volume of a sphere with radius r is (4/3)πr^3. 
+#What is the volume of a sphere with radius 5?
 import math
 print(math.pi)
 
@@ -28,3 +28,42 @@ v = (4/3) * math.pi * (r ** 3)
 print(v)
 
 print('The volume of a sphere with radius 5 has a volume of {:.2f}'.format(v))
+
+#2. Suppose the cover price of a book is $24.95, but bookstores get a 40\% discount. Shipping costs $3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for 60 copies?
+dp = (24.95-(24.95 * .4)) #discount price
+print(dp)
+sh = (3) #shipping price for first copy
+print(sh)
+ash = (.75) #shipping for additional copies
+print(ash)
+
+wholesalecost = ((dp * 60) + sh + (59 * ash))
+print(wholesalecost)
+
+print('The wholesale cost for 60 copies is {:.2f} dollars'.format(wholesalecost))
+
+#3. HELP: If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+start_time_hr = 6 + 52 / 60.0 #hour 6 + min 52 / 60min
+print(start_time_hr)
+
+easy_pace_hr = (8 + 15 / 60.0 ) / 60.0
+tempo_pace_hr = (7 + 12 / 60.0) / 60.0
+
+running_time_hr = 2 * easy_pace_hr + 3 * tempo_pace_hr
+print(running_time_hr)
+
+breakfast_hr = start_time_hr + running_time_hr
+print(breakfast_hr)
+breakfast_min = (breakfast_hr-int(breakfast_hr))*60
+print(breakfast_min)
+
+print('You would get home for breakfast at {:.0f}:{:.0f}AM'.format(breakfast_hr, breakfast_min))
+
+#4. If my average grade rises from 82 to 89. What is the percentage of the increase? Format the result as 'xx.x%'. Keep one figure after decimal point.
+gradeA = 82
+gradeB = 89
+
+gradeincrease = (((89-82) / 82) *100)
+print(gradeincrease)
+
+print('The grade percent increase is {:.1f}%'.format(gradeincrease))
