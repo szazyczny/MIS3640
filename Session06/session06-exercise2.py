0027163ee19f687d54be001d52c6a9dfbbae05d0
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-09-19 16:32:49 -0400

    Session 6 Exercises pt 2

--- Session06/session06-exercise2.py
+++ Session06/session06-exercise2.py
@@ -30,17 +30,18 @@ def fibonacci(n):
 
 print(fibonacci(6)) #equal to 8
 
+
+#NEED TO REDO THESE
 #2.3 The greatest common divisor of two positive integers is the largest integer that divides each of them without remainder.
-The greatest common divisor of two positive integers is the largest integer that divides each of them without remainder. For example,
-gcd(2, 12) = 2
-gcd(6, 12) = 6
-gcd(9, 12) = 3
-gcd(17, 12) = 1
-A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
-If b = 0, then the answer is a
-Otherwise, gcd(a, b) is the same as gcd(b, a % b)
-Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one integer.
-'''
+#The greatest common divisor of two positive integers is the largest integer that divides each of them without remainder. For example,
+# gcd(2, 12) = 2
+# gcd(6, 12) = 6
+# gcd(9, 12) = 3
+# gcd(17, 12) = 1
+# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive integers:
+# If b = 0, then the answer is a
+# Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+# Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one integer.
 
 
 def gcdRecur(a, b):
@@ -54,7 +55,8 @@ def gcdRecur(a, b):
         return a
     else:
         return gcdRecur(b, a%b)
-    '''
+
+
 #2.4 Tower of Hanoi
 def moveTower(height,fromPole, toPole, withPole):
     if height >= 1:
