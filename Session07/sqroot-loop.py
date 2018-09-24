46bd349d0a642f46335b470c76d54d59a5ade84e
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-09-23 22:00:46 -0400

    session 7 exercise sqrt

--- Session07/sqroot-loop.py
+++ Session07/sqroot-loop.py
@@ -1,61 +1,42 @@
-# #Copy the loop from above and encapsulate it in a function 
-# #called mysqrt that takes a as a parameter, chooses a 
-# # reasonable value of x, and returns an estimate of the 
-# # square root of a.
+#Copy the loop from above and encapsulate it in a function 
+#called mysqrt that takes a as a parameter, chooses a 
+# reasonable value of x, and returns an estimate of the 
+# square root of a.
 
-# #To test it, write a function named test_square_root that prints a table
+#To test it, write a function named test_square_root that prints a table
 
-# #NEWTONS ALGORITHM      y = (x + a/x) / 2
-
-# a = 4
-# x = 3
-# y = (x + a/x) / 2
-# print(y)
-
-# x = y
-# y = (x + a/x) / 2
-# print(y)
-
-# x = y
-# y = (x + a/x) / 2
-# print(y)
-
-# x = y
-# y = (x + a/x) / 2
-# print(y)
-
-# x = y
-# y = (x + a/x) / 2
-# print(y)
-
-# x = y
-# y = (x + a/x) / 2
-# print(y)
-
+#NEWTONS ALGORITHM      y = (x + a/x) / 2
 
 
 epsilon=0.0000001
 
 def mysqrt(a):
+    '''
+    calculates square root for given value a
+    '''
     x = 1
     while True:
         y = (x + a/x) / 2
         if abs(y-x) < epsilon:
             break
         x = y
-    print('the square root is:', x)
-    
+    print(x)
+
 for a in range (1,10):
       mysqrt(a)
 
-# epsilon=0.0000001
+# import math 
+# gen_row = lambda: [ a, mysqrt(a), math.sqrt, math.sqrt - mysqrt for c in range(4) ]
+# # 'data' is a nested list comprised of five rows and three columns
+# data = [gen_row() for c in range(1,10)]
 
-# def mysqrt(a):
-#     while True:
-#         x = a^(1/2)
-#         y = (x + a/x) / 2
-#         if abs(y-x) < epsilon:
-#             break
-#         print('the square root is:', x)
+# # create some column headers
+# col_headers = ["cl{}".format(i) for i in range(1, 10)]
 
-# mysqrt(4)
+# # print the column headers first
+# print("{:^8} {:^8} {:^8}".format(*col_headers)
+
+# # now print the rows
+# for row in 
+#   aligned_row = "{:^8} {:^8} {:^8}".format(*row)
+#   print(aligned_row)
