90ebb28fd37bd5b2187fbf09c9371cd673ccd524
szazyczny <35873673+szazyczny@users.noreply.github.com>
2018-11-12 21:42:03 -0500

    Exam Practice

--- SessionPractice/Session4/functions.py
+++ SessionPractice/Session4/functions.py
@@ -1,26 +1,37 @@
-def print_lyrics():
-    print('Hey Jude')
-    print('dont make it bad')
+# def print_lyrics():
+#     print('Hey Jude')
+#     print('dont make it bad')
 
-type(print_lyrics)
-print_lyrics()
+# type(print_lyrics)
+# print_lyrics()
 
-def repeat_lyrics():
-    print_lyrics()
-    print('Na na')
-    print_lyrics()
+# def repeat_lyrics():
+#     print_lyrics()
+#     print('Na na')
+#     print_lyrics()
 
-repeat_lyrics()
+# repeat_lyrics()
 
-def print_twice(name):
-    print(name)
-    print(name)
+# def print_twice(name):
+#     print(name)
+#     print(name)
 
-print_twice('sar')
+# print_twice('sar')
 
-my_name = 'dj zaz'
-print_twice(my_name)
+# my_name = 'dj zaz'
+# print_twice(my_name)
 
 #def my_abs(x, h, k):
 
 #def cat_twice
+
+from math import * 
+def quad(a, b, c):
+    numA = ((b**2)-(4*a*c))
+    numSQ = sqrt(numA)
+    numB = ((-b)+(numSQ)-(numSQ))
+    den = 2*a
+    x = numB / den
+    return x
+
+print(quad(3, 4 ,5))
---
+++ SessionPractice/Session5/conditionals.py
@@ -0,0 +1,28 @@
+age = 20
+if age >= 18:
+    print('Your age is', age)
+    print('adult')
+
+age = 3
+if age >= 18:
+    print('Your age is', age)
+    print('adult')
+else:
+    print('Your age is', age)
+    print('teen')
+
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6: 
+    print('teen')
+else:
+    print('kid')
+
+age = 20
+if age >= 6:
+    print('teen')
+elif age >= 18: 
+    print('adult')
+else:
+    print('kid')
